0ca6aa50a2cae4637cef824adf890f974b64db2f
claytoniousfunk <cbennett0715@gmail.com>
2024-04-10 00:22:06 -0500

    fix command

--- HI_MC/PYTHIA8_HYDJET_DiJet/PYTHIA8_HYDJET_DiJet_GEN-SIM.sh
+++ HI_MC/PYTHIA8_HYDJET_DiJet/PYTHIA8_HYDJET_DiJet_GEN-SIM.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cmsRun PYTHIA8_HYDJET_DiJet_genConfig.py \
-       --mc \
-       --pileup HiMixGEN \
-       --pileup_input "dbs:/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbAutumn18GS-103X_upgrade2018_realistic_HI_v11-v1/GEN-SIM" \
-       --eventcontent RAWSIM \
-       --datatier GEN-SIM \
-       --conditions 103X_upgrade2018_realistic_HI_v11 \
-       --beamspot MatchHI \
-       --step GEN,SIM \
-       --scenario HeavyIons \
-       --geometry DB:Extended \
-       --era Run2_2018_pp_on_AA
+cmsDriver.py PYTHIA8_HYDJET_DiJet_genConfig.py \
+	     --mc \
+	     --pileup HiMixGEN \
+	     --pileup_input "dbs:/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbAutumn18GS-103X_upgrade2018_realistic_HI_v11-v1/GEN-SIM" \
+	     --eventcontent RAWSIM \
+	     --datatier GEN-SIM \
+	     --conditions 103X_upgrade2018_realistic_HI_v11 \
+	     --beamspot MatchHI \
+	     --step GEN,SIM \
+	     --scenario HeavyIons \
+	     --geometry DB:Extended \
+	     --era Run2_2018_pp_on_AA
