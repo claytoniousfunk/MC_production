d890dfd453837005d2384b61989a7cdb5062cb5b
claytoniousfunk <cbennett0715@gmail.com>
2024-04-10 00:42:40 -0500

    syntax fix

--- HI_MC/PYTHIA8_HYDJET_DiJet/PYTHIA8_HYDJET_DiJet_GEN-SIM.sh
+++ HI_MC/PYTHIA8_HYDJET_DiJet/PYTHIA8_HYDJET_DiJet_GEN-SIM.sh
@@ -2,7 +2,7 @@
 
 cmsDriver.py Configuration/GenProduction/python/PYTHIA8_HYDJET_DiJet_genFragment.py \
 	     --python_filename PYTHIA8_HYDJET_DiJet_GEN-SIM.py \
-	     --fileout /eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_GEN-SIM/PYTHIA8_HYDJET_DiJet_GEN-SIM.root
+	     --fileout /eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_GEN-SIM/PYTHIA8_HYDJET_DiJet_GEN-SIM.root \
 	     --mc \
 	     --pileup HiMixGEN \
 	     --pileup_input "dbs:/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbAutumn18GS-103X_upgrade2018_realistic_HI_v11-v1/GEN-SIM" \
