5876d9cf929b00a2fab086d927d0dc80b0dad889
claytoniousfunk <cbennett0715@gmail.com>
2024-05-16 15:50:20 -0500

    fix

--- HI_MC/PYTHIA8_HYDJET_DiJet_5020GeV/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.sh
+++ HI_MC/PYTHIA8_HYDJET_DiJet_5020GeV/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.sh
@@ -5,6 +5,8 @@ GLOBAL_TAG="103X_upgrade2018_realistic_HI_v11"
 ERA="Run2_2018_pp_on_AA"
 INPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RAW/PYTHIA8_HYDJET_DiJet_5020GeV_RAW.root"
 OUTPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RECO/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.root"
+NUMBER_OF_THREADS=8
+NUMBER_OF_EVENTS=100
 
 cmsDriver.py step2 \
 	     --mc \
@@ -13,12 +15,12 @@ cmsDriver.py step2 \
 	     --datatier GEN-SIM-RECO \
 	     --conditions $GLOBAL_TAG \
 	     --step RAW2DIGI,L1Reco,RECO \
-	     --nThreads $NUMBER_OF_THREADS \
 	     --scenario HeavyIons \
 	     --geometry DB:Extended \
 	     --era $ERA \
 	     --filein file:$INPUT_FILE \
 	     --fileout file:$OUTPUT_FILE \
+	     --nThreads $NUMBER_OF_THREADS \
 	     --no_exec \
 	     -n $NUMBER_OF_EVENTS 
 
