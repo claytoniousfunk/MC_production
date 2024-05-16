b2470bc2515f3f446b8bdf5af6ee6121d3cc6fee
claytoniousfunk <cbennett0715@gmail.com>
2024-05-16 16:03:41 -0500

    put step2 back.  Lower to 4 threads

--- HI_MC/PYTHIA8_HYDJET_DiJet_5020GeV/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.sh
+++ HI_MC/PYTHIA8_HYDJET_DiJet_5020GeV/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.sh
@@ -5,10 +5,11 @@ GLOBAL_TAG="103X_upgrade2018_realistic_HI_v11"
 ERA="Run2_2018_pp_on_AA"
 INPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RAW/PYTHIA8_HYDJET_DiJet_5020GeV_RAW.root"
 OUTPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RECO/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.root"
-NUMBER_OF_THREADS=8
+NUMBER_OF_THREADS=4
 NUMBER_OF_EVENTS=100
 
-cmsDriver.py --mc \
+cmsDriver.py step2 \
+	     --mc \
 	     --python_filename $OUTPUT_EXECUTABLE \
 	     --eventcontent AODSIM \
 	     --datatier GEN-SIM-RECO \
