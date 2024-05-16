687089c96890da44391072fd66f7bb72ad6fff92
claytoniousfunk <cbennett0715@gmail.com>
2024-05-16 17:36:46 -0500

    update

--- HI_MC/PYTHIA8_HYDJET_DiJet_5020GeV/PYTHIA8_HYDJET_DiJet_5020GeV_RAW.sh
+++ HI_MC/PYTHIA8_HYDJET_DiJet_5020GeV/PYTHIA8_HYDJET_DiJet_5020GeV_RAW.sh
@@ -15,7 +15,7 @@ cmsDriver.py step1 \
 	     --pileup HiMix \
 	     --pileup_input "dbs:/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbAutumn18GS-103X_upgrade2018_realistic_HI_v11-v1/GEN-SIM" \
 	     --eventcontent RAWSIM \
-	     --datatier GEN-SIM-DIGI-RAW \
+	     --datatier GEN-SIM-RAW \
 	     --conditions $GLOBAL_TAG \
 	     --step DIGI:pdigi_hi_nogen,L1,DIGI2RAW,HLT:HIon \
 	     --scenario HeavyIons \
