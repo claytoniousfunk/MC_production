1f1a330792fce90cb6c972e4cdee3cb210b9ebd4
claytoniousfunk <cbennett0715@gmail.com>
2024-05-16 12:48:07 -0500

    initial commit

---
+++ HI_MC/PYTHIA8_HYDJET_DiJet_5360GeV/PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cmsDriver.py Configuration/GenProduction/python/PYTHIA8_HYDJET_DiJet_5360GeV_genFragment.py \
+	     --python_filename PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM.py \
+	     --fileout file:/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_GEN-SIM/PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM.root \
+	     --mc \
+	     --pileup HiMixGEN \
+	     --pileup_input "dbs:/MinBias_Drum5F_5p36TeV_hydjet/HINPbPbSpring23GS-130X_mcRun3_2023_realistic_HI_v18-v2/GEN-SIM" \
+	     --eventcontent RAWSIM \
+	     --datatier GEN-SIM \
+	     --conditions 130X_mcRun3_2023_realistic_HI_v18 \
+	     --beamspot MatchHI \
+	     --step GEN,SIM \
+	     --scenario HeavyIons \
+	     --geometry DB:Extended \
+	     --era Run3_pp_on_PbPb \
+	     --nThreads 8 \
+	     --no_exec
+	     -n 100
+	     
+
+
+
+
+
+
+
+
+
+
+
