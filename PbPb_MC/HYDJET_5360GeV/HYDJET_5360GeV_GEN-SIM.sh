1ca22a74c40db3812565215f43602cc4d67c4628
claytoniousfunk <cbennett0715@gmail.com>
2024-05-23 00:26:11 -0500

    initialize new directory for HYDJET production

---
+++ PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_GEN-SIM.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+GEN_FRAGMENT_PATH="Configuration/GenProduction/python/PYTHIA8_HYDJET_DiJet_5360GeV_genFragment.py"
+OUTPUT_EXECUTABLE="PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM.py"
+OUTPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_GEN-SIM/PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM.root"
+GLOBAL_TAG="130X_mcRun3_2023_realistic_HI_v18"
+BEAMSPOT="MatchHI"
+ERA="Run3_pp_on_PbPb"
+NUMBER_OF_THREADS=8
+NUMBER_OF_EVENTS=100
+
+# CMSSW release: CMSSW_13_0_18_HeavyIon
+cmsDriver.py $GEN_FRAGMENT_PATH \
+	     --python_filename $OUTPUT_EXECUTABLE \
+	     --fileout file:$OUTPUT_FILE \
+	     --mc \
+	     --pileup HiMixGEN \
+	     --pileup_input "dbs:/MinBias_Drum5F_5p36TeV_hydjet/HINPbPbSpring23GS-130X_mcRun3_2023_realistic_HI_v18-v2/GEN-SIM" \
+	     --eventcontent RAWSIM \
+	     --datatier GEN-SIM \
+	     --conditions $GLOBAL_TAG \
+	     --beamspot $BEAMSPOT \
+	     --step GEN,SIM \
+	     --scenario HeavyIons \
+	     --geometry DB:Extended \
+	     --era  $ERA\
+	     --nThreads $NUMBER_OF_THREADS \
+	     --no_exec \
+	     -n $NUMBER_OF_EVENTS
+	     
+
+
+
+
+
+
+
+
+
+
+
