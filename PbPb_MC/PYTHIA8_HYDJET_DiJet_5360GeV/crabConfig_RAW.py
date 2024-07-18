ba7e5dd554830018b7e4427dc770dbbbde78efc1
claytoniousfunk <cbennett0715@gmail.com>
2024-07-18 00:12:01 -0500

    changes

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RAW.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RAW.py
@@ -2,27 +2,27 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RAW_PH_DiJet_pThat-30to120_batch4'
+config.General.requestName = 'crabSubmit_RAW_PYTHIA8_HYDJET_DiJet_5360GeV_2024-07-17'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5020GeV_RAW_1.py'
+config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5020GeV_RAW.py'
 config.JobType.maxMemoryMB = 4000
 config.JobType.maxJobRuntimeMin = 2700
 #config.JobType.allowUndistributedCMSSW = True
 
 config.section_("Data")
 
-config.Data.inputDataset = '/PH_DiJet_pThat-30to120/phys_heavyions-PH_DiJet_pThat-30to120_MC_generation-f625d404341f14e17a13cc48895c09a0/USER'
+config.Data.inputDataset = '/PYTHIA8_HYDJET_DiJet_5360GeV/phys_heavyions-PYTHIA8_HYDJET_DiJet_5360GeV_MC_generation-03531bc68739f23c6678e8f42003e014/USER'
 config.Data.inputDBS = 'phys03'
 #config.Data.outputPrimaryDataset = 'PH_DiJet_pThat-30to120'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PH_DiJet_pThat-30to120_MC_generation'
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_MC_generation'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
