2ee139570a8badce3cf151b6bea2a53e0409b167
claytoniousfunk <cbennett0715@gmail.com>
2024-07-18 01:21:18 -0500

    fix pileup and GT

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/PYTHIA8_HYDJET_DiJet_5360GeV_RAW.sh
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/PYTHIA8_HYDJET_DiJet_5360GeV_RAW.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 OUTPUT_EXECUTABLE="PYTHIA8_HYDJET_DiJet_5360GeV_RAW.py"
-GLOBAL_TAG="140X_mcRun3_2024_realistic_v3"
+GLOBAL_TAG="141X_mcRun3_2024_realistic_HI_v2"
 ERA="Run3_pp_on_PbPb_2023"
 INPUT_FILE="PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM.root"
 OUTPUT_FILE="PYTHIA8_HYDJET_DiJet_5360GeV_RAW.root"
 NUMBER_OF_THREADS=1
 NUMBER_OF_EVENTS=100
 
-#CMSSW release: CMSSW_13_2_10
+#CMSSW release: CMSSW_14_1_0_pre3
 cmsDriver.py step1 \
 	     --python_filename $OUTPUT_EXECUTABLE \
 	     --mc \
 	     --pileup HiMix \
-	     --pileup_input "dbs:/MB_TuneCP5_5p36TeV_ppref-pythia8_CMSSW_14_0_0-140X_mcRun3_2024_realistic_v3_GENSIM/phys_heavyions-ppRefMC_20240219_140X_MB_GEN_SIM-657d236c02e738e7f424d8f0e6db5784/USER instance=prod/phys03" \
+	     --pileup_input "dbs:/Hydjet_Drum5F_CMSW_14_1_0_pre3_22May2024_v1/phys_heavyions-GEN_MC_Hydjet_Drum5F_CMSW_14_1_0_pre3_22May2024_v1-c1c381bf911200feb914e6f29e1be6f7/USER instance=prod/phys03" \
 	     --eventcontent RAWSIM \
 	     --datatier GEN-SIM-DIGI-RAW \
 	     --conditions $GLOBAL_TAG \
