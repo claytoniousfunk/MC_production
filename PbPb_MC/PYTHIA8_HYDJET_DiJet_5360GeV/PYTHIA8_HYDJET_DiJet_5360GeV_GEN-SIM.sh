aee4d7d8ff1fbb512737487e49f7de780fc1fa5c
claytoniousfunk <cbennett0715@gmail.com>
2024-08-22 12:34:25 -0500

    fix CMSSW release in comment line

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM.sh
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM.sh
@@ -9,7 +9,7 @@ ERA="Run3_pp_on_PbPb"
 NUMBER_OF_THREADS=1
 NUMBER_OF_EVENTS=10
 
-# CMSSW release: CMSSW_14_1_0
+# CMSSW release: CMSSW_14_1_0_pre3
 cmsDriver.py $GEN_FRAGMENT_PATH \
 	     --python_filename $OUTPUT_EXECUTABLE \
 	     --fileout $OUTPUT_FILE \
