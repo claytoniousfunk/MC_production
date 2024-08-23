61b125144c2753d011f3dac8ef3078032257b09c
claytoniousfunk <cbennett0715@gmail.com>
2024-08-23 10:04:26 -0500

    change unitsPerJob to 100 (try to reduce failures)

--- PbPb_MC/HYDJET_5360GeV/crabConfig_GEN-SIM.py
+++ PbPb_MC/HYDJET_5360GeV/crabConfig_GEN-SIM.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_GEN-SIM_HYDJET_5360GeV_141X_2024-08-22'
+config.General.requestName = 'crabSubmit_GEN-SIM_HYDJET_5360GeV_141X_2024-08-23'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -15,11 +15,11 @@ config.JobType.psetName = 'HYDJET_5360GeV_GEN-SIM.py'
 config.section_("Data")
 config.Data.outputPrimaryDataset = 'HYDJET_5360GeV_141X'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 500
+config.Data.unitsPerJob = 100
 NJOBS = 10  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
-config.Data.outputDatasetTag = 'HYDJET_5360GeV_GEN-SIM_141X_2024-08-22'
+config.Data.outputDatasetTag = 'HYDJET_5360GeV_GEN-SIM_141X_2024-08-23'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
