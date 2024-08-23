4b0e21323c7e9128c6b2c1e9f68a2af92147e490
claytoniousfunk <cbennett0715@gmail.com>
2024-08-23 12:02:30 -0500

    try Automatic splitting

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RAW.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RAW.py
@@ -18,9 +18,9 @@ config.section_("Data")
 config.Data.inputDataset = '/PYTHIA8_HYDJET_DiJet_5360GeV_141X/phys_heavyions-PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM_141X_2024-08-20-c6831afe9537f9464bc43859c7e31f00/USER'
 config.Data.inputDBS = 'phys03'
 #config.Data.outputPrimaryDataset = 'PH_DiJet_pThat-30to120'
-config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 100
-config.Data.totalUnits = -1
+config.Data.splitting = 'Automatic'
+#config.Data.unitsPerJob = 100
+#config.Data.totalUnits = -1
 config.Data.publication = True
 config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RAW_141X_2024-08-23'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
