2478b7b2c89114371b0eea4dd9467d160feb24ab
claytoniousfunk <cbennett0715@gmail.com>
2024-08-27 13:47:55 -0500

    change date in dataset name

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RAW.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RAW.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RAWDEBUG_PYTHIA8_HYDJET_DiJet_5360GeV_141X_2024-08-26'
+config.General.requestName = 'crabSubmit_RAWDEBUG_PYTHIA8_HYDJET_DiJet_5360GeV_141X_2024-08-27'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -20,7 +20,7 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RAWDEBUG_141X_2024-08-26'
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RAWDEBUG_141X_2024-08-27'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
