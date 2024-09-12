c880a28c1641f86d646fa5fc22df9b329c0dbb82
claytoniousfunk <cbennett0715@gmail.com>
2024-09-12 13:37:06 -0500

    fix input/output and n_evt

--- pp_MC/PYTHIA8_DiJet_5360GeV/PYTHIA8_DiJet_5360GeV_RECO.sh
+++ pp_MC/PYTHIA8_DiJet_5360GeV/PYTHIA8_DiJet_5360GeV_RECO.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-INPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RAW/PYTHIA8_DiJet_5360GeV_RAW.root"
+INPUT_FILE="PYTHIA8_DiJet_5360GeV_RAW.root"
 RECO_CONFIG="PYTHIA8_DiJet_5360GeV_RECO.py"
-OUTPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RECO/PYTHIA8_DiJet_5360GeV_RECO.root"
+OUTPUT_FILE="PYTHIA8_DiJet_5360GeV_RECO.root"
 GLOBAL_TAG="140X_mcRun3_2024_realistic_v3"
-NUMBER_OF_THREADS=8
+NUMBER_OF_THREADS=1
 ERA="Run3"
-NUMBER_OF_EVENTS=10000
+NUMBER_OF_EVENTS=10
 
 # STEP 3: RECO (MiniAOD)
 cmsDriver.py --filein file:$INPUT_FILE \
