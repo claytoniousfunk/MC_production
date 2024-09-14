3ecd4e1df839860dad82ef48c1bd29a25cf3a852
claytoniousfunk <cbennett0715@gmail.com>
2024-09-14 12:24:50 -0500

    configure for 132X

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/PYTHIA8_HYDJET_DiJet_5360GeV_miniAOD.sh
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/PYTHIA8_HYDJET_DiJet_5360GeV_miniAOD.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 OUTPUT_EXECUTABLE="PYTHIA8_HYDJET_DiJet_5360GeV_miniAOD.py"
-GLOBAL_TAG="141X_mcRun3_2024_realistic_HI_v2"
+GLOBAL_TAG="132X_mcRun3_2023_realistic_HI_v9"
 ERA="Run3_pp_on_PbPb_2023"
-INPUT_FILE="PYTHIA8_HYDJET_DiJet_5360GeV_RECO.root"
-OUTPUT_FILE="PYTHIA8_HYDJET_DiJet_5360GeV_miniAOD.root"
+INPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/test/2023_PbPb/PYTHIA8_HYDJET_DiJet_5360GeV_RECODEBUG.root"
+OUTPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/test/2023_PbPb/PYTHIA8_HYDJET_DiJet_5360GeV_miniAOD.root"
 NUMBER_OF_THREADS=1
-NUMBER_OF_EVENTS=100
+NUMBER_OF_EVENTS=10
 
-# CMSSW release: CMSSW_14_1_0_pre3
+# CMSSW release: CMSSW_13_2_10
 cmsDriver.py --mc \
 	     --python_filename $OUTPUT_EXECUTABLE \
 	     --eventcontent MINIAODSIM \
