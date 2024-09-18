e367d0e63e070394aacb0c1862d5e0931cc3c1a1
claytoniousfunk <cbennett0715@gmail.com>
2024-09-18 09:01:13 -0500

    change name strings

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RECO.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RECO.py
@@ -2,13 +2,13 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RECO_PYTHIA8_HYDJET_DiJet_5360GeV_132X_2024-09-18'
+config.General.requestName = 'crabSubmit_RECODEBUG_PYTHIA8_HYDJET_DiJet_5360GeV_132X_2024-09-18'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5360GeV_RECO.py'
+config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5360GeV_RECODEBUG.py'
 config.JobType.maxMemoryMB = 4000
 config.JobType.maxJobRuntimeMin = 2700
 #config.JobType.allowUndistributedCMSSW = True
@@ -22,7 +22,7 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RECO_132X_2024-09-18'
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RECODEBUG_132X_2024-09-18'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
