4629facb7b17b4b8b2b14c71524f70c334912871
claytoniousfunk <cbennett0715@gmail.com>
2024-09-18 07:42:45 -0500

    new D0->PiPi setup

---
+++ pp_MC/D0ToPiPi_Pthat15_PT-4_TuneCP5_5p02TeV_pythia8/GEN-SIM.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+GEN_FRAGMENT_PATH="Configuration/GenProduction/python/Pythia8_d0_fragment_PiPi.py"
+GEN_CONFIG="Pythia8_d0_PiPi_GEN-SIM.py"
+OUTPUT_FILE="Pythia8_d0_PiPi_GEN-SIM.root"
+GLOBAL_TAG="auto:phase1_2017_realistic"
+BEAMSPOT="Realistic25ns13TeVEarly2017Collision"
+NUMBER_OF_THREADS=1
+ERA="Run2_2017"
+NUMBER_OF_EVENTS=10
+
+# STEP 1: GEN-SIM
+cmsDriver.py $GEN_FRAGMENT_PATH \
+	     --python_filename $GEN_CONFIG \
+	     --fileout file:$OUTPUT_FILE \
+	     --step GEN,SIM \
+	     --datatier GEN-SIM \
+	     --eventcontent RAWSIM \
+	     --mc \
+	     --conditions $GLOBAL_TAG \
+	     --beamspot $BEAMSPOT \
+	     --nThreads $NUMBER_OF_THREADS \
+	     --geometry DB:Extended \
+	     --era $ERA \
+	     --no_exec \
+	     -n $NUMBER_OF_EVENTS
