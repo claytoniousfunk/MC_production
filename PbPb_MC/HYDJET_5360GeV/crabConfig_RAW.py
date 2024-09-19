3b8c1f9700a84f723c62f479f10ccac80ce6e5b4
claytoniousfunk <cbennett0715@gmail.com>
2024-09-19 08:14:04 -0500

    configure RAW crab configs

--- PbPb_MC/HYDJET_5360GeV/crabConfig_RAW.py
+++ PbPb_MC/HYDJET_5360GeV/crabConfig_RAW.py
@@ -8,20 +8,20 @@ config.General.transferOutputs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5360GeV_RAWDEBUG.py'
+config.JobType.psetName = 'HYDJET_5360GeV_RAWDEBUG.py'
 config.JobType.maxMemoryMB = 4000
 config.JobType.maxJobRuntimeMin = 2700
 
 config.section_("Data")
 
-config.Data.inputDataset = '/PYTHIA8_HYDJET_DiJet_5360GeV_130X/phys_heavyions-PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM_130X_2024-09-14-2bcea9d9a630e1684665574799557bab/USER'
+config.Data.inputDataset = '/HYDJET_5360GeV_141X/phys_heavyions-HYDJET_5360GeV_GEN-SIM_141X_2024-09-18-28c13d839895327199b411cd5cc6d217/USER'
 config.Data.inputDBS = 'phys03'
 
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RAWDEBUG_132X_2024-09-14'
+config.Data.outputDatasetTag = 'HYDJET_5360GeV_RAWDEBUG_141X_2024-09-19'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RAW.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RAW.py
@@ -2,26 +2,26 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RAWDEBUG_HYDJET_5360GeV_141X_2024-09-19'
+config.General.requestName = 'crabSubmit_RAWDEBUG_PYTHIA8_HYDJET_DiJet_5360GeV_132X_2024-09-14'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'HYDJET_5360GeV_RAWDEBUG.py'
+config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5360GeV_RAWDEBUG.py'
 config.JobType.maxMemoryMB = 4000
 config.JobType.maxJobRuntimeMin = 2700
 
 config.section_("Data")
 
-config.Data.inputDataset = ''
+config.Data.inputDataset = '/PYTHIA8_HYDJET_DiJet_5360GeV_130X/phys_heavyions-PYTHIA8_HYDJET_DiJet_5360GeV_GEN-SIM_130X_2024-09-14-2bcea9d9a630e1684665574799557bab/USER'
 config.Data.inputDBS = 'phys03'
 
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'HYDJET_5360GeV_RAWDEBUG_141X_2024-09-19'
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RAWDEBUG_132X_2024-09-14'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
