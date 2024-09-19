6be49b666da38c4fa47c77253df42c355e3a6c96
claytoniousfunk <cbennett0715@gmail.com>
2024-09-19 08:10:09 -0500

    RAWDEBUG

---
+++ PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_RAWDEBUG.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+GEN_FRAGMENT_PATH="Configuration/GenProduction/python/HYDJET_5360GeV_genFragment.py"
+OUTPUT_EXECUTABLE="HYDJET_5360GeV_RAW.py"
+GLOBAL_TAG="141X_mcRun3_2024_realistic_HI_v2"
+ERA="Run3_pp_on_PbPb"
+INPUT_FILE="HYDJET_5360GeV_GEN-SIM.root"
+OUTPUT_FILE="HYDJET_5360GeV_RAW.root"
+NUMBER_OF_THREADS=1
+NUMBER_OF_EVENTS=10
+
+#CMSSW release: CMSSW_14_1_0_pre3
+cmsDriver.py $GEN_FRAGMENT_PATH \
+	     --python_filename $OUTPUT_EXECUTABLE \
+	     --mc \
+	     --pileup HiMixNoPU \
+	     --eventcontent FEVTDEBUGHLT \
+	     --datatier GEN-SIM-DIGI-RAW-HLTDEBUG \
+	     --conditions $GLOBAL_TAG \
+	     --step DIGI:pdigi_hi_nogen,L1,DIGI2RAW,HLT:HIon \
+	     --geometry DB:Extended \
+	     --era $ERA \
+	     --filein file:$INPUT_FILE \
+	     --fileout file:$OUTPUT_FILE \
+	     --nThreads $NUMBER_OF_THREADS \
+	     --no_exec \
+	     -n $NUMBER_OF_EVENTS
+
+
+
+
