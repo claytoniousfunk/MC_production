8372cf0931424d062a568abdc7e41d1aa9dfdf44
claytoniousfunk <cbennett0715@gmail.com>
2024-09-19 08:06:12 -0500

    bring to 141X

--- PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_RAW.sh
+++ PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_RAW.sh
@@ -2,14 +2,14 @@
 
 GEN_FRAGMENT_PATH="Configuration/GenProduction/python/HYDJET_5360GeV_genFragment.py"
 OUTPUT_EXECUTABLE="HYDJET_5360GeV_RAW.py"
-GLOBAL_TAG="132X_mcRun3_2023_realistic_HI_v9"
-ERA="Run3_pp_on_PbPb_2023"
-INPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_GEN-SIM/HYDJET_5360GeV/HYDJET_5360GeV_GEN-SIM.root"
-OUTPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RAW/HYDJET_5360GeV/HYDJET_5360GeV_RAW.root"
-NUMBER_OF_THREADS=8
-NUMBER_OF_EVENTS=100
+GLOBAL_TAG="141X_mcRun3_2024_realistic_HI_v2"
+ERA="Run3_pp_on_PbPb"
+INPUT_FILE="HYDJET_5360GeV_GEN-SIM.root"
+OUTPUT_FILE="HYDJET_5360GeV_RAW.root"
+NUMBER_OF_THREADS=1
+NUMBER_OF_EVENTS=10
 
-#CMSSW release: CMSSW_13_2_10
+#CMSSW release: CMSSW_14_1_0_pre3
 cmsDriver.py $GEN_FRAGMENT_PATH \
 	     --python_filename $OUTPUT_EXECUTABLE \
 	     --mc \
