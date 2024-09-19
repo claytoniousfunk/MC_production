885d233fadd5fdbd0be108f56bfe23eec61a166e
claytoniousfunk <cbennett0715@gmail.com>
2024-09-19 16:44:28 -0500

    rename directory

---
+++ pp_MC/D0ToPiPi_Pthat15_PT-4_TuneCP5_5p02TeV_pythia8-evtgen/RAW.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INPUT_FILE="Pythia8_d0_PiPi_GEN-SIM.root"
+RAW_CONFIG="Pythia8_d0_PiPi_RAW.py"
+OUTPUT_FILE="Pythia8_d0_PiPi_RAW.root"
+GLOBAL_TAG="94X_mc2017_realistic_forppRef5TeV"
+NUMBER_OF_THREADS=1
+ERA="Run2_2017_ppRef"
+NUMBER_OF_EVENTS=10
+
+# STEP 2: RAW-DIGI
+cmsDriver.py --filein file:$INPUT_FILE \
+	     --python_filename $RAW_CONFIG \
+	     --fileout file:$OUTPUT_FILE \
+	     --step DIGI,L1,DIGI2RAW,HLT:PRef \
+	     --datatier GEN-SIM-RAW \
+	     --eventcontent RAWSIM \
+	     --mc \
+	     --conditions $GLOBAL_TAG \
+	     --pileup_input "dbs:/MinBias_TuneCUETP8M1_2017_5p02TeV-pythia8/RunIIpp5Spring18GS-94X_mc2017_realistic_v10For2017G_v3-v2/GEN-SIM" \
+	     --pileup_dasoption="--limit=0" \
+	     --pileup E7TeV_AVE_2_BX2808 \
+	     --nThreads $NUMBER_OF_THREADS \
+	     --geometry DB:Extended \
+	     --era $ERA \
+	     --no_exec \
+	     -n $NUMBER_OF_EVENTS
