423927d1f5244072c6e992be9578414d8546edd5
claytoniousfunk <cbennett0715@gmail.com>
2024-09-24 16:25:28 -0500

    add convertPythiCodes declare to false

--- PbPb_MC/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen_genFragment.py
+++ PbPb_MC/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen_genFragment.py
@@ -14,7 +14,8 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
                 operates_on_particles = cms.vint32(),
                 particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt.pdl'),
                 user_decay_file = cms.vstring('GeneratorInterface/ExternalDecays/data/D0_Kpi.dec'),
-                list_forced_decays = cms.vstring('myD0', 'myanti-D0')
+                list_forced_decays = cms.vstring('myD0', 'myanti-D0'),
+                convertPythiaCodes=cms.untracked.bool(False)
                 ),
             parameterSets = cms.vstring('EvtGen130')
             ),
