7559f8131c8138a2a3fd2d420f9f119b905536e3
claytoniousfunk <cbennett0715@gmail.com>
2024-09-24 16:40:04 -0500

    do -1 (all) events

--- PbPb_MC/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen_RECO.sh
+++ PbPb_MC/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen_RECO.sh
@@ -6,7 +6,7 @@ ERA="Run2_2018_pp_on_AA"
 INPUT_FILE="promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen_RAW.root"
 OUTPUT_FILE="promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen_RECO.root"
 NUMBER_OF_THREADS=1
-NUMBER_OF_EVENTS=100
+NUMBER_OF_EVENTS=-1
 
 # CMSSW release: CMSSW_10_3_2
 cmsDriver.py step2 \
