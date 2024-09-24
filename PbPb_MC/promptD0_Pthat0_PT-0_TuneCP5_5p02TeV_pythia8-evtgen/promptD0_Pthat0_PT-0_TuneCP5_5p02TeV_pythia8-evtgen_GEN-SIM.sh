f78ce3f25bdea6804593adadfb8593b7a76c4807
claytoniousfunk <cbennett0715@gmail.com>
2024-09-24 14:15:04 -0500

    fix pileup_input

--- PbPb_MC/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen_GEN-SIM.sh
+++ PbPb_MC/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen/promptD0_Pthat0_PT-0_TuneCP5_5p02TeV_pythia8-evtgen_GEN-SIM.sh
@@ -15,7 +15,7 @@ cmsDriver.py $GEN_FRAGMENT_PATH \
 	     --fileout $OUTPUT_FILE \
 	     --mc \
 	     --pileup HiMixGEN \
-	     --pileup_input "dbs:/MinBias_Drum5F_5p36TeV_hydjet/HINPbPbSpring23GS-130X_mcRun3_2023_realistic_HI_v18-v2/GEN-SIM instance=prod/phys03" \
+	     --pileup_input "dbs:/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbAutumn18GS-103X_upgrade2018_realistic_HI_v11-v1/GEN-SIM instance=prod/phys03" \
 	     --eventcontent RAWSIM \
 	     --datatier GEN-SIM \
 	     --conditions $GLOBAL_TAG \
