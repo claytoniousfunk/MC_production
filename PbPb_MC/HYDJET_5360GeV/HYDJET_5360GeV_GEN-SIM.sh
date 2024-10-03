9627283f33e9a4a43e22f3dc26282f7cc81f647c
claytoniousfunk <cbennett0715@gmail.com>
2024-10-02 20:04:13 -0500

    run on 130X / 132X (2023 PbPb settings)

--- PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_GEN-SIM.sh
+++ PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_GEN-SIM.sh
@@ -3,13 +3,13 @@
 GEN_FRAGMENT_PATH="Configuration/GenProduction/python/HYDJET_5360GeV_genFragment.py"
 OUTPUT_EXECUTABLE="HYDJET_5360GeV_GEN-SIM.py"
 OUTPUT_FILE="HYDJET_5360GeV_GEN-SIM.root"
-GLOBAL_TAG="141X_mcRun3_2024_realistic_HI_v2"
+GLOBAL_TAG="130X_mcRun3_2023_realistic_HI_v18"
 BEAMSPOT="Realistic2023PbPbCollision"
 ERA="Run3_pp_on_PbPb"
 NUMBER_OF_THREADS=1
 NUMBER_OF_EVENTS=10
 
-# CMSSW release: CMSSW_14_1_0_pre3
+# CMSSW release: CMSSW_13_0_20_HeavyIon
 cmsDriver.py $GEN_FRAGMENT_PATH \
 	     --python_filename $OUTPUT_EXECUTABLE \
 	     --fileout file:$OUTPUT_FILE \
--- PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_RAWDEBUG.sh
+++ PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_RAWDEBUG.sh
@@ -2,14 +2,14 @@
 
 GEN_FRAGMENT_PATH="Configuration/GenProduction/python/HYDJET_5360GeV_genFragment.py"
 OUTPUT_EXECUTABLE="HYDJET_5360GeV_RAWDEBUG.py"
-GLOBAL_TAG="141X_mcRun3_2024_realistic_HI_v2"
+GLOBAL_TAG="132X_mcRun3_2023_realistic_HI_v9"
 ERA="Run3_pp_on_PbPb"
 INPUT_FILE="HYDJET_5360GeV_GEN-SIM.root"
 OUTPUT_FILE="HYDJET_5360GeV_RAWDEBUG.root"
 NUMBER_OF_THREADS=1
 NUMBER_OF_EVENTS=10
 
-#CMSSW release: CMSSW_14_1_0_pre3
+#CMSSW release: CMSSW_13_2_10
 cmsDriver.py $GEN_FRAGMENT_PATH \
 	     --python_filename $OUTPUT_EXECUTABLE \
 	     --mc \
