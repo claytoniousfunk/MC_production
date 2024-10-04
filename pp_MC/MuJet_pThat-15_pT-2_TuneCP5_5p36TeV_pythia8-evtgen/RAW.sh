28bb3a8474a7bfb26ae8a0973477d2e995a585dc
claytoniousfunk <cbennett0715@gmail.com>
2024-10-04 11:13:24 -0500

    new RAW script

---
+++ pp_MC/MuJet_pThat-15_pT-2_TuneCP5_5p36TeV_pythia8-evtgen/RAW.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+INPUT_FILE="MuJet_pThat-15_pT-2_TuneCP5_5p36TeV_pythia8-evtgen_GEN-SIM.root"
+RAW_CONFIG="MuJet_pThat-15_pT-2_TuneCP5_5p36TeV_pythia8-evtgen_RAW.py"
+OUTPUT_FILE="MuJet_pThat-15_pT-2_TuneCP5_5p36TeV_pythia8-evtgen_RAW.root"
+GLOBAL_TAG="141X_mcRun3_2024_realistic_ppRef5TeV_v4"
+ERA="Run3"
+NUMBER_OF_THREADS=1
+NUMBER_OF_EVENTS=-1
+
+# STEP 2: RAW-DIGI (Flat 0 to 4, BX=25ns)
+cmsDriver.py --filein file:$INPUT_FILE \
+	     --python_filename $RAW_CONFIG \
+	     --fileout file:$OUTPUT_FILE \
+	     --step DIGI,L1,DIGI2RAW,HLT:@fake2 \
+	     --datatier GEN-SIM-RAW \
+	     --eventcontent RAWSIM \
+	     --mc \
+	     --conditions $GLOBAL_TAG \
+	     --pileup_input "dbs:/MB_TuneCP5_5p36TeV_ppref-pythia8_CMSSW_14_0_0-140X_mcRun3_2024_realistic_v3_GENSIM/phys_heavyions-ppRefMC_20240219_140X_MB_GEN_SIM-657d236c02e738e7f424d8f0e6db5784/USER instance=prod/phys03" \
+	     --pileup_dasoption="--limit=0" \
+	     --pileup Flat_10_50_25ns \
+	     --customise_commands "process.mix.input.nbPileupEvents.probFunctionVariable = cms.vint32(0,1,2,3,4) \n process.mix.input.nbPileupEvents.probValue = cms.vdouble(0.200000,0.200000,0.200000,0.200000,0.200000)" \
+	     --nThreads $NUMBER_OF_THREADS \
+	     --geometry DB:Extended \
+	     --era $ERA \
+	     --no_exec \
+	     -n $NUMBER_OF_EVENTS
