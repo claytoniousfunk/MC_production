6125a0267b4b4027d517f2c4cf87c033da5878fc
claytoniousfunk <cbennett0715@gmail.com>
2024-10-04 18:46:48 -0500

    new RAW config for 103X MC

---
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_RAW.py
@@ -0,0 +1,28 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'crabSubmit_RAW_PYTHIA8_HYDJET_DiJet_5020GeV_103X_2024-10-04'
+config.General.workArea = config.General.requestName
+config.General.transferOutputs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5020GeV_RAW.py'
+config.JobType.maxMemoryMB = 4000
+config.JobType.maxJobRuntimeMin = 2700
+
+config.section_("Data")
+
+config.Data.inputDataset = '/PYTHIA8_HYDJET_DiJet_5020GeV_103X/phys_heavyions-PYTHIA8_HYDJET_DiJet_5020GeV_GEN-SIM_103X_2024-10-04-f64465610d99fff38e0b14c94f679813/USER'
+config.Data.inputDBS = 'phys03'
+
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.publication = True
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_RAW_103X_2024-10-04'
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
+
+config.section_("Site")
+config.Site.storageSite = 'T2_CH_CERN'
