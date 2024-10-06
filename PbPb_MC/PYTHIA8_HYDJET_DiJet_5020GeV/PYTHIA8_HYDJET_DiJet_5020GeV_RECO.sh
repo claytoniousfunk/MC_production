f61e817ab154e42112b87c24325b7201195dc85f
claytoniousfunk <cbennett0715@gmail.com>
2024-10-06 12:37:29 -0500

    configure for RECO 103X

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.sh
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.sh
@@ -3,10 +3,10 @@
 OUTPUT_EXECUTABLE="PYTHIA8_HYDJET_DiJet_5020GeV_RECO.py"
 GLOBAL_TAG="103X_upgrade2018_realistic_HI_v11"
 ERA="Run2_2018_pp_on_AA"
-INPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RAW/PYTHIA8_HYDJET_DiJet_5020GeV_RAW.root"
-OUTPUT_FILE="/eos/cms/store/group/phys_heavyions/cbennett/MC_production/output_RECO/PYTHIA8_HYDJET_DiJet_5020GeV_RECO.root"
-NUMBER_OF_THREADS=8
-NUMBER_OF_EVENTS=10000
+INPUT_FILE="PYTHIA8_HYDJET_DiJet_5020GeV_RAW.root"
+OUTPUT_FILE="PYTHIA8_HYDJET_DiJet_5020GeV_RECO.root"
+NUMBER_OF_THREADS=1
+NUMBER_OF_EVENTS=-1
 
 # CMSSW release: CMSSW_10_3_2
 cmsDriver.py step2 \
---
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_RECO.py
@@ -0,0 +1,29 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'crabSubmit_RECO_PYTHIA8_HYDJET_DiJet_5020GeV_103X_2024-10-06'
+config.General.workArea = config.General.requestName
+config.General.transferOutputs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5020GeV_RECO.py'
+config.JobType.maxMemoryMB = 4000
+config.JobType.maxJobRuntimeMin = 2700
+#config.JobType.allowUndistributedCMSSW = True
+
+config.section_("Data")
+
+config.Data.inputDataset = '/PYTHIA8_HYDJET_DiJet_5020GeV_103X/phys_heavyions-PYTHIA8_HYDJET_DiJet_5020GeV_RAW_103X_2024-10-04-d57f5e81b90a7ad5c8b6d5beabc7b953/USER'
+config.Data.inputDBS = 'phys03'
+#config.Data.outputPrimaryDataset = 'PH_DiJet_pThat-30to120'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.publication = True
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RECO_103X_2024-10-06'
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
+
+config.section_("Site")
+config.Site.storageSite = 'T2_CH_CERN'
