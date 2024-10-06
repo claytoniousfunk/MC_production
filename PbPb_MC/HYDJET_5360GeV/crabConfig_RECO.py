1287e7418a1b404c6acf3634f4fb9bd2c22049f8
claytoniousfunk <cbennett0715@gmail.com>
2024-10-06 12:25:17 -0500

    stage for 132X

--- PbPb_MC/HYDJET_5360GeV/crabConfig_RECO.py
+++ PbPb_MC/HYDJET_5360GeV/crabConfig_RECO.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RECODEBUG_HYDJET_5360GeV_141X_2024-09-23'
+config.General.requestName = 'crabSubmit_RECODEBUG_HYDJET_5360GeV_132X_2024-10-06'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -15,13 +15,13 @@ config.JobType.maxJobRuntimeMin = 2700
 
 config.section_("Data")
 
-config.Data.inputDataset = '/HYDJET_5360GeV_141X/phys_heavyions-HYDJET_5360GeV_RAWDEBUG_141X_2024-09-19-a233780aca207b23c01035d3323b7388/USER'
+config.Data.inputDataset = '/HYDJET_5360GeV_130X/phys_heavyions-HYDJET_5360GeV_RAWDEBUG_132X_2024-10-05-f7f07afd712c727c521ecee0bfa87d82/USER'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'HYDJET_5360GeV_RECODEBUG_141X_2024-09-23'
+config.Data.outputDatasetTag = 'HYDJET_5360GeV_RECODEBUG_132X_2024-10-06'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
