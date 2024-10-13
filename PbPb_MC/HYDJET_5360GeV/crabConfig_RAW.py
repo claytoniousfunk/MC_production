a617a19e87a201d3ea6ae68b5aaa2d73f696d07f
claytoniousfunk <cbennett0715@gmail.com>
2024-10-12 20:52:23 -0500

    new date in name

--- PbPb_MC/HYDJET_5360GeV/crabConfig_RAW.py
+++ PbPb_MC/HYDJET_5360GeV/crabConfig_RAW.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RAWDEBUG_HYDJET_5360GeV_132X_2024-10-05'
+config.General.requestName = 'crabSubmit_RAWDEBUG_HYDJET_5360GeV_132X_2024-10-12'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -21,7 +21,7 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'HYDJET_5360GeV_RAWDEBUG_132X_2024-10-05'
+config.Data.outputDatasetTag = 'HYDJET_5360GeV_RAWDEBUG_132X_2024-10-12'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
