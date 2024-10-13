35b352d0547c7b5f1f46ae3e57e75263945daabb
claytoniousfunk <cbennett0715@gmail.com>
2024-10-12 21:02:37 -0500

    run with updated era

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RECO.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5360GeV/crabConfig_RECO.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RECODEBUG_PYTHIA8_HYDJET_DiJet_5360GeV_132X_2024-09-23'
+config.General.requestName = 'crabSubmit_RECODEBUG_updatedEra_PYTHIA8_HYDJET_DiJet_5360GeV_132X_2024-10-12'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -22,7 +22,7 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RECODEBUG_132X_2024-09-23'
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5360GeV_RECODEBUG_updatedEra_132X_2024-10-12'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
