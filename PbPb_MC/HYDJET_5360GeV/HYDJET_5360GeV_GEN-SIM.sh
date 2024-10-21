b009bd86251728e48b6277e61e658c938296a005
claytoniousfunk <cbennett0715@gmail.com>
2024-10-21 09:55:07 -0500

    update configuration settings

--- PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_GEN-SIM.sh
+++ PbPb_MC/HYDJET_5360GeV/HYDJET_5360GeV_GEN-SIM.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# general info
 GEN_FRAGMENT_PATH="Configuration/GenProduction/python/HYDJET_5360GeV_genFragment.py"
 OUTPUT_EXECUTABLE="HYDJET_5360GeV_GEN-SIM.py"
 OUTPUT_FILE="HYDJET_5360GeV_GEN-SIM.root"
+# detector configurations
 GLOBAL_TAG="141X_mcRun3_2024_realistic_HI_v8"
-BEAMSPOT="Realistic2023PbPbCollision"
-ERA="Run3_pp_on_PbPb"
+BEAMSPOT="DBrealistic"
+ERA="Run3_pp_on_PbPb_2024"
+# number of events
 NUMBER_OF_THREADS=1
 NUMBER_OF_EVENTS=10
 
-# CMSSW release: CMSSW_13_0_20_HeavyIon
+# CMSSW release: CMSSW_14_1_0_pre3
 cmsDriver.py $GEN_FRAGMENT_PATH \
 	     --python_filename $OUTPUT_EXECUTABLE \
 	     --fileout file:$OUTPUT_FILE \
