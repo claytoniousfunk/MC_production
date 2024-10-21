b51717daccc2241703bf112fb63794d186fa4f04
claytoniousfunk <cbennett0715@gmail.com>
2024-10-21 09:57:12 -0500

    new output name

--- PbPb_MC/HYDJET_5360GeV/crabConfig_GEN-SIM.py
+++ PbPb_MC/HYDJET_5360GeV/crabConfig_GEN-SIM.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_GEN-SIM_HYDJET_5360GeV_141X_updatedGT_2024-10-20-2'
+config.General.requestName = 'crabSubmit_GEN-SIM_HYDJET_5360GeV_141X_updatedGT_2024-10-21'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -19,7 +19,7 @@ config.Data.unitsPerJob = 10
 NJOBS = 500  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
-config.Data.outputDatasetTag = 'HYDJET_5360GeV_GEN-SIM_141X_updatedGT_2024-10-20'
+config.Data.outputDatasetTag = 'HYDJET_5360GeV_GEN-SIM_141X_updatedGT_2024-10-21'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
