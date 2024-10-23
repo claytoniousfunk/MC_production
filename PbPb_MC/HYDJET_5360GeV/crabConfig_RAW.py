d78906617c0d7c7226dbaea8697c71bfd9f94e70
claytoniousfunk <cbennett0715@gmail.com>
2024-10-23 00:50:31 -0500

    run RAW with updated Era and GT

--- PbPb_MC/HYDJET_5360GeV/crabConfig_RAW.py
+++ PbPb_MC/HYDJET_5360GeV/crabConfig_RAW.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RAWDEBUG_HYDJET_5360GeV_141X_updatedGT_2024-10-21'
+config.General.requestName = 'crabSubmit_RAWDEBUG_HYDJET_5360GeV_141X_updatedEra_updatedGT_2024-10-23'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -14,14 +14,14 @@ config.JobType.maxJobRuntimeMin = 2700
 
 config.section_("Data")
 
-config.Data.inputDataset = '/HYDJET_5360GeV_141X/phys_heavyions-HYDJET_5360GeV_GEN-SIM_141X_updatedGT_2024-10-21-85590546038c8ee9b15975a270d8a133/USER'
+config.Data.inputDataset = '/HYDJET_5360GeV_141X/phys_heavyions-HYDJET_5360GeV_GEN-SIM_141X_updatedEra_updatedGT-fbdb411e1d56c29ce543392cf295fa81/USER'
 config.Data.inputDBS = 'phys03'
 
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'HYDJET_5360GeV_RAWDEBUG_141X_updatedGT_2024-10-21'
+config.Data.outputDatasetTag = 'HYDJET_5360GeV_RAWDEBUG_141X_updatedEra_updatedGT'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
