39c043f21a498035c8aa6e04d5d7b7c4850a8247
claytoniousfunk <cbennett0715@gmail.com>
2024-11-21 09:46:28 -0600

    run raw over batch 1

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_RAW.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_RAW.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RAW_PYTHIA8_HYDJET_DiJet_5020GeV_103X_2024-10-17-3'
+config.General.requestName = 'crabSubmit_RAW_PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch1_2024-11-21'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -14,15 +14,15 @@ config.JobType.maxJobRuntimeMin = 2700
 
 config.section_("Data")
 
-config.Data.inputDataset = '/PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch15/cbennett-PYTHIA8_HYDJET_DiJet_5020GeV_GEN-SIM_103X_2024-10-17-3-f64465610d99fff38e0b14c94f679813/USER'
+config.Data.inputDataset = '/PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch1/phys_heavyions-PYTHIA8_HYDJET_DiJet_5020GeV_GEN-SIM_103X_batch1_2024-11-20-1fef5af90597ac1d8724c7d2fc8854dc/USER'
 config.Data.inputDBS = 'phys03'
 
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_RAW_103X_2024-10-17-3'
-#config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_RAW_103X_batch1_2024-11-21'
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
-config.Site.storageSite = 'T2_US_Vanderbilt'
+config.Site.storageSite = 'T2_CH_CERN'
