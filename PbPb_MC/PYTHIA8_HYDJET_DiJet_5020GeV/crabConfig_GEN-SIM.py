b799a2fddfa1ab37bc737104c84c0e0d6a17d87c
claytoniousfunk <cbennett0715@gmail.com>
2025-01-19 11:53:31 -0600

    run gen-sim over batch 12

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_GEN-SIM.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_GEN-SIM.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_GEN-SIM_PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch11_2025-01-14'
+config.General.requestName = 'crabSubmit_GEN-SIM_PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch12_2025-01-19'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -13,13 +13,13 @@ config.JobType.psetName = 'PYTHIA8_HYDJET_DiJet_5020GeV_GEN-SIM.py'
 #config.JobType.allowUndistributedCMSSW = True
 
 config.section_("Data")
-config.Data.outputPrimaryDataset = 'PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch11'
+config.Data.outputPrimaryDataset = 'PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch12'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 1000
+config.Data.unitsPerJob = 800
 NJOBS = 5000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_GEN-SIM_103X_batch11_2025-01-14'
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_GEN-SIM_103X_batch12_2025-01-19'
 
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 config.section_("Site")
