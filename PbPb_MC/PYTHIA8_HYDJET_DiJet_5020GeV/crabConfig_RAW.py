c47b6c9002596cdfd3b8efe4e199a494b9d4eb40
claytoniousfunk <cbennett0715@gmail.com>
2025-02-09 12:15:55 -0600

    retry raw over batch 13

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_RAW.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_RAW.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_RAW_PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch13_2025-01-27'
+config.General.requestName = 'crabSubmit_RAW_PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch13_2025-02-09'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -21,7 +21,7 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_RAW_103X_batch13_2025-01-27'
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_RAW_103X_batch13_2025-02-09'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 
 config.section_("Site")
