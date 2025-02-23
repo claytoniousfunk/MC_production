7294182e9cd99efca27eae1cd1d300801d0665fe
claytoniousfunk <cbennett0715@gmail.com>
2025-02-23 00:20:55 -0600

    retry

--- PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_GEN-SIM.py
+++ PbPb_MC/PYTHIA8_HYDJET_DiJet_5020GeV/crabConfig_GEN-SIM.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'crabSubmit_GEN-SIM_PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch16_2025-02-21'
+config.General.requestName = 'crabSubmit_GEN-SIM_PYTHIA8_HYDJET_DiJet_5020GeV_103X_batch16_2025-02-23'
 config.General.workArea = config.General.requestName
 config.General.transferOutputs = True
 
@@ -19,7 +19,7 @@ config.Data.unitsPerJob = 900
 NJOBS = 5000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
-config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_GEN-SIM_103X_batch16_2025-02-21'
+config.Data.outputDatasetTag = 'PYTHIA8_HYDJET_DiJet_5020GeV_GEN-SIM_103X_batch16_2025-02-23'
 
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
 config.section_("Site")
