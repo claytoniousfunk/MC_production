e0fd80fac7e6af148f724b21ff5e1ef28c0e7e18
clayton <clayton@debian>
2025-08-26 14:10:26 -0500

    set nThreads = 1

--- pp_MC/MinBias_TuneCP5_5p36TeV-pythia8/MinBias_TuneCP5_5p36TeV-pythia8_RECO.sh
+++ pp_MC/MinBias_TuneCP5_5p36TeV-pythia8/MinBias_TuneCP5_5p36TeV-pythia8_RECO.sh
@@ -5,7 +5,7 @@ GLOBAL_TAG="141X_mcRun3_2024_realistic_ppRef5TeV_v7"
 ERA="Run3_2024_ppRef"
 INPUT_FILE="MinBias_TuneCP5_5p36TeV-pythia8_RAW.root"
 OUTPUT_FILE="MinBias_TuneCP5_5p36TeV-pythia8_RECO.root"
-NUMBER_OF_THREADS=2
+NUMBER_OF_THREADS=1
 NUMBER_OF_EVENTS=-1
 
 # CMSSW release: CMSSW_14_1_9
