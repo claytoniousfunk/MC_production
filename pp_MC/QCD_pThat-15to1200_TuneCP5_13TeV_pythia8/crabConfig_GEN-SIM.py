802228c573f52fbccd48417b13a266e415be5989
clayton <clayton@debian>
2025-09-08 13:37:42 -0500

    new files for 13 TeV pythia8

---
+++ pp_MC/QCD_pThat-15to1200_TuneCP5_13TeV_pythia8/crabConfig_GEN-SIM.py
@@ -0,0 +1,27 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'crabSubmit_GEN-SIM_QCD_pThat-15to1200_TuneCP5_13TeV_pythia8_2025-09-08'
+config.General.workArea = config.General.requestName
+config.General.transferOutputs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'QCD_pThat-15to1200_TuneCP5_13TeV_pythia8_GEN-SIM.py'
+#config.JobType.maxMemoryMB = 4000
+#config.JobType.allowUndistributedCMSSW = True
+
+config.section_("Data")
+config.Data.outputPrimaryDataset = 'QCD_pThat-15to1200_TuneCP5_13TeV_pythia8'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 500
+NJOBS = 20000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.publication = True
+config.Data.outputDatasetTag = 'QCD_pThat-15to1200_TuneCP5_13TeV_pythia8_GEN-SIM_2025-09-08'
+
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/cbennett/'+config.General.requestName
+config.section_("Site")
+config.Site.storageSite = 'T2_CH_CERN'
+
